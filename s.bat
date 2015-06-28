0e56fa2128a177ff58b015df5248021187646f87
Arno Gau <tetranoir@users.noreply.github.com>
2015-06-28 12:24:39 -0700

    Create s.bat

---
+++ s.bat
@@ -0,0 +1,16 @@
+@echo off
+
+setlocal enableDelayedExpansion
+set home=C:\Users\blahblah\Documents\Coding\
+set bs=bravesoul
+set pa3=C++\CSE100\PA3
+set pa4=C++\CSE100\PA4
+set 150=CSE150
+set 151=CSE151
+set 110=CSE110
+set flashy=CSE110
+
+cd "%home%\!%1!"
+pushd .
+endlocal
+popd
